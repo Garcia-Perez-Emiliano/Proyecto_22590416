0f7689dde55997ed4a262b28cb10ba7119f76cab
emiga <l22590416@sjuanrio.tecnm.mx>
2026-02-17 20:43:31 -0600

    Actualizacion 1

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+#Cargar las variables de entorno
+load_dotenv()
 
 #crear instancia
 app =  Flask(__name__)
 
 # Configuración de la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://emiliano:wX52vqqKUABTfkPNELVdLPtGODPuUtKi@dpg-d65uqgi48b3s73ei54vg-a.oregon-postgres.render.com/cetech_g835'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
